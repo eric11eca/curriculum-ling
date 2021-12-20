a440ec09ceadfdfbcf721495c2385ce17507dcbe
Zeming Chen <chenz16@rose-hulman.edu>
2021-12-19 22:06:29 -0500

    add useful batched files for inocculation and generalization experiments

---
+++ inoculation.bat
@@ -0,0 +1,95 @@
+python main_probing.py --main_loop --task_name hypernymy --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name hypernymy --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name hyponymy --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name hyponymy --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name transitive --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name transitive --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name ner --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name ner --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name verbcorner --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name verbcorner --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name verbnet --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name verbnet --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name syntactic_alternation --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name syntactic_alternation --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name monotonicity_infer --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name monotonicity_infer --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name syllogism --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name syllogism --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name puns --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name puns --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name sentiment --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name sentiment --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name kg_relations --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name kg_relations --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name context_align --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name context_align --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name coreference --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name coreference --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name sprl --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name sprl --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name atomic --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name atomic --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name social_chem --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name social_chem --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name socialqa --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name socialqa --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name physicalqa --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name physicalqa --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name entailment_tree --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name entailment_tree --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name proof_writer --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name proof_writer --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name counterfactual --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name counterfactual --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name temporal --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name temporal --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name spatial --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name spatial --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name drop --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name drop --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name logiqa --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name logiqa --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name cosmoqa --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name cosmoqa --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name conditional --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name conditional --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name comparative --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name comparative --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name counting --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name counting --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name quantifier --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name quantifier --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
+
+python main_probing.py --main_loop --task_name negation --model roberta-anli-mix --k_shot 50 --tokenize
+::python main_probing.py --main_loop --task_name negation --model roberta-anli-mix --k_shot 50 --tokenize  --null_baseline
